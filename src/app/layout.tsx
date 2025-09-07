f9f2d26a931a22aa3800fab059bc6321bb6d4b3b
Zeel <deltapixel8a@gmail.com>
2025-09-07 23:29:17 +0530

    ZeelScript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@
 
 // export const metadata: Metadata = {
 //   title: "Zeel Jasani | Portfolio",
-//   description: "Portfolio of Zeel Jasani - Web Developer",
+//   description: "Zeel Jasani - Software Developer",
 // };
 
 // export default function RootLayout({
@@ -28,6 +28,9 @@
 // }>) {
 //   return (
 //     <html lang="en" className="scroll-behavior-smooth">
+//       <head>
+//         <link rel="icon" href="/zeel.ico" />
+//       </head>
 //       <body className={twMerge(
 //         "min-h-screen text-white font-sans antialiased",
 //         inter.variable,
@@ -51,16 +54,33 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import type { Metadata } from "next";
-import { Inter, Calistoga } from 'next/font/google'
+import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 import { Header } from "@/sections/Header";
 import { Footer } from "@/sections/Footer";
 
 const inter = Inter({
-  subsets: ["latin"], 
-  variable: "--font-sans"
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 const calistoga = Calistoga({
@@ -70,31 +90,44 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "Zeel Jasani | Portfolio",
+  title: "Zeel Jasani - Software Developer",
   description: "Zeel Jasani - Software Developer",
+  icons: {
+    icon: "https://jasanizeel4827.github.io/ZeelScript/zeellogo.png",
+    shortcut: "https://jasanizeel4827.github.io/ZeelScript/zeellogo.png",
+    apple: "https://jasanizeel4827.github.io/ZeelScript/zeellogo.png",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className="scroll-behavior-smooth">
+    <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="icon" href="/zeel.ico" />
+        <link
+          rel="icon"
+          href="https://jasanizeel4827.github.io/ZeelScript/zeellogo.png"
+          type="image/png"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="https://jasanizeel4827.github.io/ZeelScript/zeellogo.png"
+        />
       </head>
-      <body className={twMerge(
-        "min-h-screen text-white font-sans antialiased",
-        inter.variable,
-        calistoga.variable,
-        "bg-[#121927]"
-      )}>
+      <body
+        className={twMerge(
+          "min-h-screen text-white font-sans antialiased",
+          inter.variable,
+          calistoga.variable,
+          "bg-[#121927]"
+        )}
+      >
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">
-            {children}
-          </main>
+          <main className="flex-grow">{children}</main>
           <Footer />
         </div>
       </body>
